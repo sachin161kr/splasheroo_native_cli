c941536057b45e8702283c88cabfea40e2315631
Sachin Kumar <mail.sachin161@gmail.com>
2023-03-14 15:22:11 +0530

    SplashScreen and Icons Added

---
+++ android/app/src/main/java/com/splasheroo/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.splasheroo;
+
+import  android.content.Intent;
+import  android.os.Bundle;
+
+import  androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
